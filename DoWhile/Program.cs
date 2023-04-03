6427fb119e038386771733baee45eea7859fdb5e
sahinmelikov <1rima621melikov@gmail.com>
2023-04-04 01:33:11 +0400

    Do While Loop

---
+++ DoWhile/Program.cs
@@ -0,0 +1,42 @@
+﻿int eded = 19;
+
+if (eded >= 10 && eded <= 20)
+{
+    if (eded % 2 == 0)
+    {
+        Console.WriteLine(eded + "" + "Cut ededdir");
+    }
+    else
+    {
+        Console.WriteLine(eded + "" + "Tek ededdir");
+    }
+}
+else
+{
+    Console.WriteLine("error");
+}
+//int eded = 25;
+//if (eded >= 10 && eded <= 20)
+//{
+
+//    do
+//    {
+
+//        if (eded % 2 == 0)
+//        {
+//            Console.WriteLine(eded + "" + "Cut ededdir");
+//        }
+//        else
+
+//            Console.WriteLine(eded + "" + "Tek ededdir");
+//        eded++;
+
+//    } while (eded <= 20);
+
+//}
+//else
+//{
+//    Console.WriteLine("Eroor");
+//}
+
+//Console.ReadLine();
